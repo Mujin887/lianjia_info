0203d6a7db079f4ce37772d7a2fc6a3537f8c35a
scott.xie <scott.xie@runmit.com>
2016-08-29 17:08:29 +0800

    add core module

---
+++ lianjia-core/src/main/java/com/xp233/demo/lianjia/App.java
@@ -0,0 +1,13 @@
+package com.xp233.demo.lianjia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
