92424ccb50dbab4854e6e1c873ae03e1e8511a8f
scott.xie <scott.xie@runmit.com>
2017-02-08 11:23:32 +0800

    add some new info

--- crawler/lianjia.py
+++ crawler/lianjia.py
@@ -40,15 +40,16 @@ def trade_spider(area_arg):
         # print(location.string,location_url)
         location_soup = get_url_soup(location_url)
         area = location_url.split("/")[-2]
-        set_area_sum_data(area, location.string, location_soup)
+        set_district_sum_data(area, location.string, location_soup)
 
         area_link = location_soup.find_all('div',{'class':'hide'})[1]
+        # print(area_link)
         for inner_location in area_link.find_all('a')[area_offset:]:
             inner_location_url = "http://bj.lianjia.com"+inner_location.get('href')
             # print(inner_location.string,inner_location_url)
             inner_location_soup = get_url_soup(inner_location_url)
             inner_area= inner_location_url.split("/")[-2]
-            set_area_sum_data(area + ":" +inner_area, inner_location.string, inner_location_soup)
+            set_district_sum_data(area + ":" +inner_area, inner_location.string, inner_location_soup)
 
     del_result = del_pipe.execute()
     print("del_result:\n", del_result)
@@ -66,11 +67,11 @@ def get_url_soup(url):
     return BeautifulSoup(plain_text,"html.parser")
 
 '''
-    存储每个区域的房价数据
+    存储每个城市的房价数据
     存储的时候：
         key：demo:lianjia:house_price:[城市]:[区域]
         score：日期（YYYYMMDD）
-        value：[日期],[区域],[当天成交量],[当天房源带看量],[在售房源总套数],[最近90天成交套数]
+        value：1,[日期],[区域],[当天成交量],[当天房源新增],[当天客人新增],[当天房源带看量],[在售房源总套数],[最近90天成交套数]
     获取最近60天的记录的命令：
         zrange [key] -60 -1
 '''
@@ -78,13 +79,18 @@ def set_area_sum_data(area, area_string, area_soup):
     # !!!在Python中，只有模块，类以及函数才会引入新的作用域，其它的代码块是不会引入新的作用域的。!!!
     # 以下都注释掉
     #     today_sold = 0 # 当天成交量
+    #     today_add = 0 # 当天房源新增
+    #     today_customer_add = 0 # 当天客人新增
     #     today_check = 0 # 当天房源带看量
     #     on_sale = 0 # 在售房源总套数
     #     ninety_days_sold = 0 # 最近90天成交套数
 
+    data_sold = area_soup.find_all('span',{"class":"num"}) # 对城市来说，第一个是当天成交量；对区域来说只有第二和第三
+    today_sold = data_sold[0].contents[0].string
     data_sold_check = area_soup.find_all('div',{"class":"num"}) # 对城市来说，第一个是上月均价，第二个是昨日成交量，第三个是昨日房源带看量；对区域来说只有第二和第三
     if area is area_arg: # 判断是否是市级别的数据
-        today_sold = data_sold_check[1].contents[0].string
+        today_add = data_sold_check[0].contents[0].string
+        today_customer_add = data_sold_check[1].contents[0].string
         today_check = data_sold_check[2].contents[0].string
     else:
         today_sold = data_sold_check[0].contents[0].string
@@ -103,10 +109,61 @@ def set_area_sum_data(area, area_string, area_soup):
         ninety_days_sold = str(0)
 
     area_key = module_prefix + area
-    area_value = date_score + "," + area_string + "," + today_sold + "," + today_check + "," + on_sale + "," + ninety_days_sold
+    area_value = "1," + date_score + "," + area_string + "," + today_sold + "," + today_add + "," + today_customer_add + "," + today_check + "," + on_sale + "," + ninety_days_sold
     print("key: ", area_key, "\nscore: ", date_score + "\nvalue: " + area_value)
     del_pipe.zremrangebyscore(area_key, date_score, date_score)
     pipe.zadd(area_key, date_score, area_value)
+
+
+'''
+    存储每个区域的房价数据
+    存储的时候：
+        key：demo:lianjia:house_price:[城市]:[区域]
+        score：日期（YYYYMMDD）
+        value：2,[日期],[区域],[当天均价],[当天成交量],[当天房源带看量],[在售房源总套数],[最近90天成交套数]
+    获取最近60天的记录的命令：
+        zrange [key] -60 -1
+'''
+def set_district_sum_data(area, area_string, area_soup):
+    # !!!在Python中，只有模块，类以及函数才会引入新的作用域，其它的代码块是不会引入新的作用域的。!!!
+    # 以下都注释掉
+    #     today_price = 0 # 当天均价
+    #     today_sold = 0 # 当天成交量
+    #     today_check = 0 # 当天房源带看量
+    #     on_sale = 0 # 在售房源总套数
+    #     ninety_days_sold = 0 # 最近90天成交套数
+
+    data_price = area_soup.find_all('span',{"class":"num"}) # 对区域来说，第一个是当天均价
+    today_price = data_price[0].contents[0].string
+    data_sold_check = area_soup.find_all('div',{"class":"num"}) # 对区域来说，第一个是当天成交量，第二个是当天房源带看量
+    today_sold = data_sold_check[0].contents[0].string
+    today_check = data_sold_check[1].contents[0].string
+    data_sale_sold = area_soup.find_all('a',{"class":"txt"}) # 第一个是在售房源总套数，第二个是最近90天成交套数
+    on_sale = data_sale_sold[0].contents[0].string[4:-1] # string例子：在售房源1980套，将数字截取出来
+    ninety_days_sold = data_sale_sold[1].contents[0].string[10:-1] # string例子：最近90天内成交房源36051套，将数字截取出来
+
+    if str(today_price).endswith("暂无数据"):
+        today_sold = str(0)
+    if str(today_sold).endswith("暂无数据"):
+        today_sold = str(0)
+    if str(today_check).endswith("暂无数据"):
+        today_check = str(0)
+    if str(on_sale).endswith("暂无数"):
+        on_sale = str(0)
+    if str(ninety_days_sold).endswith("暂无数"):
+        ninety_days_sold = str(0)
+
+    area_key = module_prefix + area
+
+    if area.find(":") == -1:
+        area_index = "2" #区一级，丰台区，东城区等等
+    else:
+        area_index = "3" #区内一片，马家堡片区，角门片区
+    area_value = area_index + "," + date_score + "," + area_string + "," + today_price + "," + today_sold + "," + today_check + "," + on_sale + "," + ninety_days_sold
+    print("key: ", area_key, "\nscore: ", date_score + "\nvalue: " + area_value)
+    del_pipe.zremrangebyscore(area_key, date_score, date_score)
+    pipe.zadd(area_key, date_score, area_value)
+
 
 # 业务逻辑开始
 trade_spider(area_arg)
